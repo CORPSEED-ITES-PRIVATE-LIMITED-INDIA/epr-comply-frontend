da7fb37284817a326d5f0fc1708d894409346028
avnishkumar <avnish.kumar@corpseed.com>
2025-12-31 16:09:14 +0530

    some minor changes in blog detail

--- src/blog/BlogDetail.jsx
+++ src/blog/BlogDetail.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getClientBlogDetailBySlug } from "../toolkit/slices/blogSlice";
 import { useParams } from "react-router-dom";
-import img from "../assets/service2.jpg";
 import BlogFAQS from "./BlogFAQS";
 import EnquiryForm from "../components/EnquiryForm";
 
@@ -10,6 +9,7 @@ const BlogDetail = () => {
   const { blogSlug } = useParams();
   const dispatch = useDispatch();
   const blog = useSelector((state) => state.blogs.clientBlogDetail);
+  const blogList = useSelector((state) => state.blogs.clientBlogList);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -17,7 +17,7 @@ const BlogDetail = () => {
   }, [dispatch, blogSlug]);
 
   return (
-    <div className="w-full">
+    <div className="w-full bg-gray-50 dark:bg-gray-950">
       {/* ---------------- TOP BANNER ---------------- */}
       <div
         className="w-full h-[120px] flex flex-col justify-center px-8 text-white bg-gray-600"
@@ -34,32 +34,91 @@ const BlogDetail = () => {
       </div>
 
       {/* ---------------- IMAGE + FORM SECTION ---------------- */}
-      <div className="w-full flex flex-row justify-center gap-10 px-8 py-10">
-        {/* Left Image */}
-        <div className="w-[60%] h-[400px] overflow-hidden rounded-xl shadow-lg">
-          <img
-            src={blog?.image}
-            alt="blog visual"
-            className="w-full h-full object-cover"
-          />
-        </div>
+      <div className="max-w-7xl mx-auto px-6 py-12">
+        <div className="grid grid-cols-1 lg:grid-cols-[1.6fr_1fr] gap-8 items-start">
+          {/* Left Image */}
+          <div className="h-[360px] overflow-hidden rounded-2xl shadow-md">
+            <img
+              src={blog?.image}
+              alt="blog visual"
+              className="w-full h-full object-cover"
+            />
+          </div>
 
-        {/* Right Enquiry Form */}
-        <div className="w-[30%] bg-white shadow-md rounded-xl p-6">
-          <EnquiryForm />
+          {/* Right Enquiry Form */}
+          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 shadow-sm rounded-2xl p-6">
+            <EnquiryForm />
+          </div>
         </div>
       </div>
 
-      {/* ---------------- RICH TEXT HTML CONTENT ---------------- */}
-      <div className="px-8 py-10 max-w-4xl mx-auto">
-        <div
-          className="tiptap-render prose prose-lg text-left max-w-none"
-          dangerouslySetInnerHTML={{ __html: blog?.description }}
-        />
+      {/* ---------------- RICH TEXT + LATEST BLOGS ---------------- */}
+      <div className="max-w-7xl mx-auto px-6 pb-16">
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_340px] gap-10 items-start">
+          {/* LEFT: Blog Content */}
+          <div
+            className="
+              tiptap-render
+              prose
+              prose-lg
+              max-w-none
+              text-left
+              dark:prose-invert
+            "
+            dangerouslySetInnerHTML={{ __html: blog?.description }}
+          />
+
+          {/* RIGHT: Latest Blogs */}
+          <aside
+            className="
+              sticky
+              top-28
+              h-fit
+              rounded-2xl
+              border
+              border-gray-200
+              dark:border-gray-800
+              bg-white
+              dark:bg-gray-900
+              p-6
+              shadow-sm
+            "
+          >
+            <h3 className="text-xs font-semibold uppercase tracking-wider text-gray-500 dark:text-gray-400 mb-4">
+              Latest Blogs
+            </h3>
+
+            <ul className="space-y-3">
+              {blogList?.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={`/blog/${item.slug}`}
+                    className="
+                      block
+                      text-sm
+                      leading-snug
+                      font-medium
+                      text-gray-800
+                      dark:text-gray-200
+                      hover:text-green-600
+                      dark:hover:text-green-400
+                      transition-colors
+                      cursor-pointer
+                    "
+                  >
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </aside>
+        </div>
       </div>
 
       {/* ---------------- FAQ SECTION ---------------- */}
-      <BlogFAQS />
+      <div className="max-w-7xl mx-auto px-6 pb-20">
+        <BlogFAQS />
+      </div>
     </div>
   );
 };
