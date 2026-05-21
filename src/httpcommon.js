f4f98bc42b501a83617b858e4d82e91dae92763b
Aryan <aryan.naithani@corpseed.com>
2026-05-21 11:44:10 +0530

    New Next App

--- src/httpcommon.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: `/`,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-  },
-});
